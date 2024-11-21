1a211ecd1ef7e010fb2470c52658eabc9c026ef4
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-11-21 23:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-class Person{
-	constructor(name, age){
+class Person {
+	constructor(name, age) {
 		this.name = name;
-		this.age=age;
+		this.age = age;
 	}
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}	
+	greet() {
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);	
+	}
 }
 
-class Employee extends Person{
-	constructor(name, age,jobTitle){
-		super(name,age);
+class Employee extends Person {
+	constructor(name, age, jobTitle) {
+		super(name, age);
 		this.jobTitle = jobTitle;
 	}
-		jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old and my job title is ${this.jobTitle}.`)
-			
-		}
+	jobGreet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
 }
 
-const obj = new Person('Alice',25);
-obj.greet();
+const p1 = new Person('Alice', 25);
+p1.greet();
 
-const obj2 = new Employee('Bob',30,'Manager');
-obj2.jobGreet();
+const e1 = new Employee('Bob', 30, 'Manager');
+e1.jobGreet();
+
+
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
